d54beaa84a6649dc7aff66b59e91f8ab921e01b6
Lee-Roy <leeroywking@gmail.com>
2022-01-07 15:06:13 -0800

    more fixes for the heroku server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var http = require("http");
 
+let port = process.env.PORT || 3000
 http
   .createServer(function (request, response) {
       let content = "Nothing to see here"
     response.writeHead(404, { "Content-Type": "text/html" });
     response.end(content, "utf-8");
   })
-  .listen(process.env.PORT || 3000);
-console.log("Server running at http://127.0.0.1:8125/");
+  .listen(port);
+console.log("Server running at", port);
