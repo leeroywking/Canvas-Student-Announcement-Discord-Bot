811e64ece2225c2000692ccad376a8149666cd39
Lee-Roy <leeroywking@gmail.com>
2022-01-07 12:50:31 -0800

    updated with prettier for formatting

--- bot.js
+++ bot.js
@@ -6,77 +6,63 @@ const path = require("path");
 const fs = require("fs");
 const env = require("dotenv").config();
 const monitor = require("./monitor");
-const courses = require("./posts"); 
+const courses = require("./posts");
 const TOKEN = process.env.DISCORD_TOKEN;
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  //Check for commands and register 
+  //Check for commands and register
   const baseFile = "command-base.js";
   const commandBase = require(`./commands/${baseFile}`);
 
   const readCommands = (dir) => {
-
     const files = fs.readdirSync(path.join(__dirname, dir));
 
     for (const file of files) {
-
       const stat = fs.lstatSync(path.join(__dirname, dir, file));
 
       if (stat.isDirectory()) {
-
         readCommands(path.join(dir, file));
-
       }
-      //make sure that DB memory and baseFile wont be seen as a command 
+      //make sure that DB memory and baseFile wont be seen as a command
       else if (file !== baseFile && file !== "memory.db") {
-
         const option = require(path.join(__dirname, dir, file));
 
-        //sent commands to command base with the options that are entered in the custom commands like addcourse.js 
+        //sent commands to command base with the options that are entered in the custom commands like addcourse.js
         commandBase(client, option);
-        
       }
     }
   };
   readCommands("commands");
 
-
   courses(); //Called once to create table for the very first time
 
   //called every minute to get the announcements from the watchlist DB and post them
   const j = schedule.scheduleJob("*/1 * * * *", function () {
-    
     console.log("The answer to life, the universe, and everything!");
     const dbPath = path.resolve(__dirname, "./commands/db/memory.db");
 
     let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
       if (err) {
-        
         console.error(err.message);
-        
       }
-      
+
       console.log("Connected to the memory database.");
-      
     });
 
     //select channel id and course id from watchlist table so we know what course announcements should be posted on what channel
     db.serialize(() => {
       db.all(`SELECT channel_id, course_id FROM watchlist`, (err, row) => {
         if (err) {
-
           console.error(err.message);
-          
         }
 
         row.forEach((element) => {
           //if(!isNaN(element)){
-            let annURL = process.env.CANVAS_API_ANN_URL;
-            monitor(client, annURL.replace('[courseid]', element.course_id));
+          let annURL = process.env.CANVAS_API_ANN_URL;
+          monitor(client, annURL.replace("[courseid]", element.course_id));
           //}
-          
         });
       });
     });
@@ -84,13 +70,10 @@ client.on("ready", () => {
     // close the database connection
     db.close((err) => {
       if (err) {
-
         return console.error(err.message);
-
       }
 
       console.log("Close the database connection.");
-      
     });
   });
 });
--- monitor.js
+++ monitor.js
@@ -4,27 +4,22 @@ const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
 const meta = {
-
   Authorization: "Bearer " + process.env.CANVAS_TOKEN,
-
 };
 
 module.exports = (client, courseAnn) => {
   const dbPath = path.resolve(__dirname, "./commands/db/memory.db");
 
   //Api request to get announcements
-  axios.get(courseAnn, /*{ headers: meta }*/).then(function (response) {
-
+  axios.get(courseAnn /*{ headers: meta }*/).then(function (response) {
     const data = response.data;
     const status = response.status;
-    
-    //check if request is oke  
+
+    //check if request is oke
     if (status === 200) {
       let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
         if (err) {
-          
           console.error(err.message);
-          
         }
       });
 
@@ -33,9 +28,7 @@ module.exports = (client, courseAnn) => {
         //Get posted_at, course_id in a array
         db.all(`SELECT posted_at, course_id FROM courses;`, (err, row) => {
           if (err) {
-            
             console.error(err.message);
-            
           }
 
           //Reverse JSON data order from API Canvas to show oldest posts first
@@ -43,11 +36,12 @@ module.exports = (client, courseAnn) => {
 
           //Loop trough dtabase with course with all the API GET results to post on the right channel
           rev.forEach((ann) => {
-
             row.forEach((course) => {
-              if (ann.context_code.replace("course_", "") === JSON.stringify(course.course_id) && course.posted_at < ann.posted_at) {
-
-                
+              if (
+                ann.context_code.replace("course_", "") ===
+                  JSON.stringify(course.course_id) &&
+                course.posted_at < ann.posted_at
+              ) {
                 //Replace default markup html tags of canvas by discord embed syntax
                 var message = ann.message
                   .replace(/<strong>/g, "**")
@@ -59,14 +53,14 @@ module.exports = (client, courseAnn) => {
                   )
                   .replace(/\(Links to an external site.\)/g, "");
 
-                  //Build layout of embedded post
+                //Build layout of embedded post
                 const exampleEmbed = new Discord.MessageEmbed()
                   .setColor("DARK_VIVID_PINK")
                   .setTitle(ann.title)
                   .setURL(ann.url)
                   .setAuthor(
                     ann.author.display_name,
-                    ann.author.avatar_image_url, 
+                    ann.author.avatar_image_url,
                     ann.author.html_url
                   )
                   .setDescription(message.substring(0, 200))
@@ -87,36 +81,46 @@ module.exports = (client, courseAnn) => {
                   `SELECT course_id, channel_id FROM watchlist;`,
                   (err, row) => {
                     if (err) {
-
                       console.error(err.message);
-
                     }
 
                     row.forEach((record) => {
-                      if (ann.context_code.replace("course_", "") === JSON.stringify(record.course_id)) {
-                        
+                      if (
+                        ann.context_code.replace("course_", "") ===
+                        JSON.stringify(record.course_id)
+                      ) {
                         /* channel = client.channels.cache.get(record.channel_id).then(
                           channel.send(exampleEmbed)
                         ) */
-                        client.channels.fetch(record.channel_id).then(channel => { channel.send(exampleEmbed) }).catch(error => { console.log(error) });;
-
+                        client.channels
+                          .fetch(record.channel_id)
+                          .then((channel) => {
+                            channel.send(exampleEmbed);
+                          })
+                          .catch((error) => {
+                            console.log(error);
+                          });
                       }
                     });
                   }
                 );
 
                 //update DateTime so you have a reference when the last post on discord was posted, prevent posting doubles
-                db.run(`UPDATE courses SET posted_at=` + JSON.stringify(ann.posted_at) + ` WHERE course_id = ` + JSON.stringify(course.course_id)), (err, row) => {
+                db.run(
+                  `UPDATE courses SET posted_at=` +
+                    JSON.stringify(ann.posted_at) +
+                    ` WHERE course_id = ` +
+                    JSON.stringify(course.course_id)
+                ),
+                  (err, row) => {
                     if (err) {
-                      
                       console.error(err.message);
-                      
                     }
-                    
-                    console.log("Your records had been pushed to the DB succesfully!");
 
+                    console.log(
+                      "Your records had been pushed to the DB succesfully!"
+                    );
                   };
-                  
               }
             });
           });
