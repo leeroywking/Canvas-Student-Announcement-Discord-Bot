259e64f005e6bcb65522476cc32734a71942fe47
Lee-Roy <leeroywking@gmail.com>
2022-01-07 14:54:58 -0800

    prepped for heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end(content, "utf-8");
+  })
+  .listen(process.env.PORT || 3000);
+console.log("Server running at http://127.0.0.1:8125/");
